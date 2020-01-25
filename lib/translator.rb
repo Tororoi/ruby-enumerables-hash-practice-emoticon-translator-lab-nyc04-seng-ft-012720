52a1d962d8786227beaf45c28f4b619d329b35dc
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-25 01:59:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,10 +19,10 @@ end
 
 def get_japanese_emoticon(file_path,emoticon)
   result = load_library(file_path)['get_emoticon'][emoticon]
-  result ? result : "Sorry, I don't recognize that emoticon."
+  result ? result : "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path,emoticon)
   result = load_library(file_path)['get_meaning'][emoticon]
-  result ? result : "Sorry, the meaning of that is a mystery to me."
+  result ? result : "Sorry, that emoticon was not found"
 end
