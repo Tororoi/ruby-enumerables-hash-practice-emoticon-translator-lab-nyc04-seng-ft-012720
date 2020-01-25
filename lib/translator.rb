59f297c5cc40a73d5e3cbbd0075f7670bfee0f50
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-25 01:49:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,6 +12,9 @@ def load_library(file)
   emoticons.each do |key,value|
     new_hash['get_meaning'][value[1]] = key
     new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+  end
+
+  new_hash
 end
 
 def get_japanese_emoticon
